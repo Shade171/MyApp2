bf8d371e61fe68c115cd46aa1bb304b4d64e0ef8
cstark <student@cstark.local>
2016-12-15 09:58:04 -0600

    App now working!

---
+++ myApp2/OnePlayerGameViewController.swift
@@ -0,0 +1,89 @@
+//
+//  OnePlayerGameViewController.swift
+//  myApp2
+//
+//  Created by cstark on 12/14/16.
+//  Copyright © 2016 cstarkShade. All rights reserved.
+//
+
+import UIKit
+
+class OnePlayerGameViewController: UIViewController
+{
+    @IBOutlet weak var enemyLabel: UILabel!
+    @IBOutlet weak var playerLabel: UILabel!
+    @IBOutlet weak var enemyScoreLabel: UILabel!
+    @IBOutlet weak var playerScoreLabel: UILabel!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var totalWins: UILabel!
+    @IBOutlet weak var totalLosses: UILabel!
+    @IBOutlet weak var firstCardImageView: UIImageView!
+    @IBOutlet weak var firstEnemyCardImageView: UIImageView!
+    @IBOutlet weak var firstBetCardImageView: UIImageView!
+    @IBOutlet weak var firstEnemyBetCardImageView: UIImageView!
+    @IBOutlet weak var secondBetCardImageView: UIImageView!
+    @IBOutlet weak var secondEnemyBetCardImageView: UIImageView!
+    @IBOutlet weak var thirdBetCardImageView: UIImageView!
+    @IBOutlet weak var thirdEnemyBetCardImageView: UIImageView!
+    @IBOutlet weak var finalCardImageView: UIImageView!
+    @IBOutlet weak var finalEnemyCardImageView: UIImageView!
+    
+    var playerScoreTotal = 0
+    var enemyScoreTotal = 0
+    var cardNamesArray: [String] = ["aceOfSpades","aceOfDiamonds","aceOfClubs","aceOfHearts","twoOfSpades","twoOfHearts", "threeOfSpades","threeOfHearts","threeOfClubs","threeOfDiamonds", "fourOfSpades","fourOfDiamonds","fourOfClubs","fourOfHearts","fiveOfSpades","fiveOfClubs","fiveOfHearts","fiveOfDiamonds","sixOfSpades","sixOfClubs","sixOfDiamonds","sixOfHearts","sevenOfSpades","sevenOfDiamonds","sevenOfClubs","sevenOfHearts","eightOfSpades","eightOfDiamonds","eightOfClubs","eightOfHearts","nineOfSpades","nineOfDiamonds","nineOfClubs","nineOfHearts","tenOfSpades","tenOfClubs","tenOfHearts","tenOfDiamonds","jackOfSpades","jackOfDiamonds","jackOfClubs","jackOfHearts","queenOfSpades","queenOfDiamonds","queenOfClubs","queenOfHearts","kingOfSpades","kingOfDiamonds","kingOfClubs","kingOfHearts"]
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        self.playButton.setTitle("Play", for: UIControlState())
+    }
+    @IBAction func playButtonTapped(_ sender: UIButton)
+    {
+        self.playButton.setTitle("Play Round", for: UIControlState.normal)
+        
+        // Randomly chooses cards for First ImageView
+        let firstRandomNumber:Int = Int(arc4random_uniform(13))
+        
+        // Construct a string with the random number
+        let firstCardString:String = self.cardNamesArray[firstRandomNumber]
+        
+        // Set the first card image view to the asset corresponding to the randomized number
+        self.firstCardImageView.image = UIImage(named: firstCardString)
+        
+        // Second card
+        let secondRandomNumber:Int = Int(arc4random_uniform(13))
+        let secondCardString:String = self.cardNamesArray[secondRandomNumber]
+        
+        self.firstEnemyCardImageView.image = UIImage(named: secondCardString)
+        // “%@” is the wildcard character for text data in a string format.
+        
+        
+        
+        
+        //Determine the higher card - First Card
+        if firstRandomNumber > secondRandomNumber && firstCardString != secondCardString
+        {
+            //ToDo: numbers are equal
+            playerScoreTotal += 1
+            self.playerScoreLabel.text = String(playerScoreTotal)
+            
+        }
+        else if firstRandomNumber == secondRandomNumber  && firstCardString != secondCardString
+
+        {
+            let firstBetNumber:Int = Int(arc4random_uniform(13))
+            let firstBetCardString : String = self.cardNamesArray[firstBetNumber]
+          self.firstBetCardImageView.image = UIImage(named: firstBetCardString)
+        }
+        else
+        {
+            // ToDO: second card is larger
+            
+            enemyScoreTotal += 1
+            self.enemyScoreLabel.text = String(enemyScoreTotal)
+            
+            
+        }
+  
+    }
+    
+}
---
+++ myApp2/TwoPlayerViewController.swift
@@ -0,0 +1,93 @@
+//
+//  TwoPlayerViewController.swift
+//  myApp2
+//
+//  Created by cstark on 12/14/16.
+//  Copyright © 2016 cstarkShade. All rights reserved.
+//
+
+import UIKit
+
+class TwoPlayerViewController: UIViewController
+{
+    @IBOutlet weak var playerOneLabel: UILabel!
+    @IBOutlet weak var playerOneScoreLabel: UILabel!
+    @IBOutlet weak var playerOneTotalWins: UILabel!
+    @IBOutlet weak var playerTwoLabel: UILabel!
+    @IBOutlet weak var firstPlayerFirstCardImageView: UIImageView!
+    @IBOutlet weak var secondPlayerFirstCardImageView: UIImageView!
+    @IBOutlet weak var firstPlayerFirstBetCardImageView: UIImageView!
+    @IBOutlet weak var secondPlayerFirstBetCardImageView: UIImageView!
+    @IBOutlet weak var firstPlayerSecondBetCardImageView: UIImageView!
+    @IBOutlet weak var secondPlayerSecondBetCardImageView: UIImageView!
+    @IBOutlet weak var firstPlayerThirdBetCardImageView: UIImageView!
+    @IBOutlet weak var secondPlayerThirdBetCardImageView: UIImageView!
+    @IBOutlet weak var firstPlayerFinalBetCardImageView: UIImageView!
+    @IBOutlet weak var secondPlayerFinalBetCardImageView: UIImageView!
+    @IBOutlet weak var playerOnePlayButton: UIButton!
+    @IBOutlet weak var playerTwoPlayButton: UIButton!
+    @IBOutlet weak var playerTwoScoreLabel: UILabel!
+    @IBOutlet weak var playerTwoTotalWins: UILabel!
+    var playerOneScoreTotal = 0
+    var playerTwoScoreTotal = 0
+    func checkCards()
+    {
+        let firstRandomNumber:Int = Int(arc4random_uniform(13))
+        let secondRandomNumber:Int = Int(arc4random_uniform(13))
+        let firstCardString:String = self.cardNamesArray[firstRandomNumber]
+        let secondCardString:String = self.cardNamesArray[secondRandomNumber]
+        if firstRandomNumber > secondRandomNumber && firstCardString != secondCardString
+        {
+            //ToDo: numbers are equal
+            playerOneScoreTotal += 1
+            self.playerOneScoreLabel.text = String(playerOneScoreTotal)
+            
+        }
+        else if firstRandomNumber == secondRandomNumber
+        {
+            
+        }
+        else
+        {
+            // ToDO: second card is larger
+            
+            playerTwoScoreTotal += 1
+            self.playerTwoScoreLabel.text = String(playerTwoScoreTotal)
+            
+            
+        }
+        
+    }
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        self.playerOnePlayButton.setTitle("Play", for: UIControlState())
+        self.playerTwoPlayButton.setTitle("Play", for: UIControlState())
+    }
+    var cardNamesArray: [String] = ["aceOfSpades","aceOfDiamonds","aceOfClubs","aceOfHearts","twoOfSpades","twoOfHearts", "threeOfSpades","threeOfHearts","threeOfClubs","threeOfDiamonds", "fourOfSpades","fourOfDiamonds","fourOfClubs","fourOfHearts","fiveOfSpades","fiveOfClubs","fiveOfHearts","fiveOfDiamonds","sixOfSpades","sixOfClubs","sixOfDiamonds","sixOfHearts","sevenOfSpades","sevenOfDiamonds","sevenOfClubs","sevenOfHearts","eightOfSpades","eightOfDiamonds","eightOfClubs","eightOfHearts","nineOfSpades","nineOfDiamonds","nineOfClubs","nineOfHearts","tenOfSpades","tenOfClubs","tenOfHearts","tenOfDiamonds","jackOfSpades","jackOfDiamonds","jackOfClubs","jackOfHearts","queenOfSpades","queenOfDiamonds","queenOfClubs","queenOfHearts","kingOfSpades","kingOfDiamonds","kingOfClubs","kingOfHearts"]
+    @IBAction func playerOnePlayButtonTapped(_ sender: UIButton)
+    {
+        self.playerOnePlayButton.setTitle("Play Round", for: UIControlState.normal)
+        
+        // Randomly chooses cards for First ImageView
+        let firstRandomNumber:Int = Int(arc4random_uniform(13))
+        
+        // Construct a string with the random number
+        let firstCardString:String = self.cardNamesArray[firstRandomNumber]
+        
+        // Set the first card image view to the asset corresponding to the randomized number
+        self.firstPlayerFirstCardImageView.image = UIImage(named: firstCardString)
+        checkCards()
+    }
+    @IBAction func playerTwoPlayButtonTapped(_ sender: Any)
+    {
+        self.playerTwoPlayButton.setTitle("Play Round", for: UIControlState.normal)
+        
+        // Second card
+        let secondRandomNumber:Int = Int(arc4random_uniform(13))
+        let secondCardString:String = self.cardNamesArray[secondRandomNumber]
+        
+        self.secondPlayerFirstCardImageView.image = UIImage(named: secondCardString)
+        checkCards()
+    }
+}
